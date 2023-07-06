ff286990223b8add7a2520dcccf351775ba23498
Letham01 <kusanagi29@ymail.com>
2023-07-06 11:56:43 +0200

    final

--- Decorators.py
+++ Decorators.py
@@ -31,4 +31,25 @@ def get_custom_html_greeting(first, last):
 print(get_custom_html_greeting("James", "Brown"))
 print(get_custom_html_greeting(first="James", last="Brown"))
 
-#to be cont.
+#-----------------------------------------------------------------
+
+def wrap_with(tag):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            return f"<{tag}>" + func(*args, **kwargs) + f"</{tag}>"
+        return wrapper
+    return decorator
+
+@wrap_with(tag="strong")
+def get_full_name(first, last):
+    return f"{first} {last}"
+
+@wrap_with(tag="p")
+@wrap_with(tag="em")
+def get_custom_html_greeting(first, last):
+    full_name = get_full_name(first, last)
+    return f"Hello, {full_name}!"
+
+print(get_custom_html_greeting("James", "Brown"))
+print(get_custom_html_greeting(first="James", last="Brown"))
+
